5360a97f180307147fc5df1e81a20c8892c163f4
Danny Hebdon <dannhebdon@gmail.com>
2018-04-04 22:20:58 +1000

    Initial commit. Database method completed and User Model nearing completion.

---
+++ SalesInvoice/SalesInvoice/Model/Users.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Dapper;
+
+namespace SalesInvoice.Model
+{
+    public class Users
+    {
+        public long Id { get; set; }
+        public string Username { get; set; }
+        public string Pass { get; set; }
+        public string Location { get; set; } //user's store (E.G. Brisbane)
+
+        /// <summary>
+        /// Blank User constructor
+        /// </summary>
+        public Users()
+        {
+            Id = 0;
+            Username = string.Empty;
+            Pass = string.Empty;
+            Location = string.Empty;
+        }
+
+        /// <summary>
+        /// Checks to see if the database has users
+        /// </summary>
+        /// <returns>true if at least one user is registered</returns>
+        public static bool HasUsers()
+        {
+            using (var db = Utilities.Database.GetConnection())
+            {
+                var query = "SELECT COUNT(*) FROM users";
+                var results = db.ExecuteScalar(query);
+                return (long)results > 0; // returns true if the database has at least one user.                   
+            }
+        }
+
+        /// <summary>
+        /// Encryption Method to Salt and Hash passwords
+        /// </summary>
+        /// <param name="data">text to salt and hash</param>
+        /// <returns>encrypted string as var hashed.</returns>
+        public static string Encrypt(string data)
+        {
+            const string SHA_KEY = "kashdjkhadaydad89asdu90asd0aus6y8172h";
+
+            string saltedPassword = SHA_KEY + data + SHA_KEY;
+            byte[] bytes = System.Text.Encoding.ASCII.GetBytes(saltedPassword);
+            bytes = new System.Security.Cryptography.SHA256Managed().ComputeHash(bytes);
+            string hashed = System.Text.Encoding.ASCII.GetString(bytes);
+
+            return hashed;
+        }
+
+        /// <summary>
+        /// Creates user with supplied info
+        /// </summary>
+        /// <param name="username">Username of user</param>
+        /// <param name="pass">Chosen password of user</param>
+        /// <param name="location">Store location of user</param>
+        public static void CreateUser(string username, string pass, string location)
+        {
+            using (var db = Utilities.Database.GetConnection().OpenAndReturn())
+            using (var transaction = db.BeginTransaction())
+            {
+                try
+                {
+                    var query = "INSERT INTO users (username, pass, location) VALUES (@un, @up, @loc);";
+                    var param = new { un = username, up = Encrypt(pass), loc = location };
+                    var results = db.Execute(query, param, transaction);
+                    transaction.Commit();
+                }
+                catch
+                {
+                    transaction.Rollback();
+                }
+            }
+        }
+
+
+    }
+}
---
+++ SalesInvoice/SalesInvoice/Utilities/Database.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Web;
+using Dapper;
+
+namespace SalesInvoice.Utilities
+{
+    public static class Database
+    {
+        const string DB_FILE = "invoices.db";
+        const string CREATE_SCRIPT = "db.sql";
+
+        static string MAPPED_DATA;
+        static string CONNECTION_STRING;
+
+        static Database()
+        {
+            MAPPED_DATA = HttpContext.Current.Server.MapPath("~/App_Data/");
+            CONNECTION_STRING = $"Data Source={MAPPED_DATA + DB_FILE}";
+        }
+
+        /// <summary>
+        /// Create new SQLite DB Connection
+        /// </summary>
+        /// <returns>new SQLiteConnection with CONNECTION_STRING</returns>
+        public static SQLiteConnection GetConnection()
+        {
+            return new SQLiteConnection(CONNECTION_STRING);
+        }
+
+        /// <summary>
+        /// Runs to create database, using db.sql. Typically run once, on Application start.
+        /// </summary>
+        public static void CreateDatabase()
+        {
+            using (var db = GetConnection())
+            {
+                var query = System.IO.File.ReadAllText(MAPPED_DATA + CREATE_SCRIPT);
+                var results = db.Execute(query); //capture to avoid race condition issues
+            }
+        }
+    }
+}

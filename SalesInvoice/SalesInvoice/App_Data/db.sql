3b2928927949ac8fc89b3ad1aa34f5bc760f069e
Danny Hebdon <dannhebdon@gmail.com>
2018-04-04 22:33:42 +1000

    Added SQLite Create scripts for users and products. TODO orders. Unsure of how I'm going to approach the Foreign Keys, perhaps in SQL or as handled lists once data has been extracted.

---
+++ SalesInvoice/SalesInvoice/App_Data/db.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT,
+								  username TEXT NOT NULL UNIQUE,
+								  pass TEXT NOT NULL,
+								  location TEXT NOT NULL);
+
+CREATE TABLE IF NOT EXISTS products (id INTEGER PRIMARY KEY AUTOINCREMENT,
+									 productName TEXT NOT NULL UNIQUE,
+									 productPrice REAL NOT NULL);
+
+
+									 
